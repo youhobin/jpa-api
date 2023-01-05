a8756bdaa39690f2e8799ce267ff6e633af71c18
youhobin <ghdb132@naver.com>
2023-01-05 17:49:38 +0900

    OneToMany 주문 조회 - JPA 에서 DTO 직접조회 ( N+1문제)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -106,6 +106,8 @@ public class OrderRepository {
         ).getResultList();
     }
 
+
+
     public List<OrderSimpleQueryDto> findOrderDtos() {
         return em.createQuery(
                 "select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address) " +
@@ -113,6 +115,27 @@ public class OrderRepository {
                     " join o.member m" +
                     " join o.delivery d", OrderSimpleQueryDto.class)
                 .getResultList();
+
+    }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o" +
+                    " join fetch o.member m" +
+                    " join fetch o.delivery d" +
+                    " join fetch o.orderItems oi" +
+                    " join fetch oi.item i", Order.class)
+            .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                    " join fetch o.member m" +
+                    " join fetch o.delivery d", Order.class)
+            .setFirstResult(offset)
+            .setMaxResults(limit)
+            .getResultList();
 
     }
 }
